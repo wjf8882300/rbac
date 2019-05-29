2b1af652425923154a0d3ba26c6a1d7282b6f01d
王井锋 <wangjingfeng@runxsports.com>
2019-05-29 15:59:51 +0800

    过滤不必要的文件

--- src/main/java/com/tongu/rbac/security/MyFilterSecurityInterceptor.java
+++ src/main/java/com/tongu/rbac/security/MyFilterSecurityInterceptor.java
@@ -1,6 +1,7 @@
 package com.tongu.rbac.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,13 +16,18 @@ import org.springframework.security.access.intercept.AbstractSecurityInterceptor
 import org.springframework.security.access.intercept.InterceptorStatusToken;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Lists;
 
 @Component
 public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor implements Filter {
 
 	@Autowired
 	private FilterInvocationSecurityMetadataSource securityMetadataSource;
+	
+	private List<AntPathRequestMatcher> prefixIignores = null;
 
 	@Autowired
 	public void setMyAccessDecisionManager(MyAccessDecisionManager myAccessDecisionManager) {
@@ -30,14 +36,26 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-
+		if(prefixIignores == null) {
+			prefixIignores = Lists.newArrayList();
+			for(String s : new String[] {"/css/**", "/js/**", "/html/**", "/img/**"}) {
+				prefixIignores.add(new AntPathRequestMatcher(s));
+			}
+		}
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		FilterInvocation fi = new FilterInvocation(request, response, chain);
+		if(canIgnore(fi)) {
+			try {
+				fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
+				return;
+			} catch (Exception e) {
+			}
+		}
+		
 		// fi里面有一个被拦截的url
 		// 里面调用MyInvocationSecurityMetadataSource的getAttributes(Object
 		// object)这个方法获取fi对应的所有权限
@@ -49,6 +67,15 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 		} finally {
 			super.afterInvocation(token, null);
 		}
+	}
+
+	private boolean canIgnore(FilterInvocation fi) {  
+        for (AntPathRequestMatcher matcher : prefixIignores) {  
+            if (matcher.matches(fi.getHttpRequest())) {  
+                return true;  
+            }
+        }  
+        return false;
 	}
 
 	@Override
